6305c86d335dbc3539a77a1a3f10432d323bb2c4
srideviPK <sridevip_mca@ahead.students.amrita.edu>
2024-08-30 15:32:08 -0400

    Implement missing endpoints in app.js

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,6 +2,7 @@
 
 # from django.shortcuts import render
 # from django.http import HttpResponseRedirect, HttpResponse
+import requests
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
