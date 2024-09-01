7f1f9b78f47f9e2d189cc0804b5c1714cfb3287a
srideviPK <sridevip_mca2207065@ahead.students.amrita.edu>
2024-09-01 15:37:48 +0530

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -28,7 +28,8 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
+    path('get_cars/',TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html"), name='dealers'),
     path('dealer/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='dealer'),
-    
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
